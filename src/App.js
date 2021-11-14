a8ed63e3fa06fb3c074c652ab5a2034d614b5270
Rakesh K R <rakeshkumar8055@gmail.com>
2021-11-14 11:16:18 +0530

    bootstrap configuration and code cleanup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      
+      <header>
+        Hack Ideas
+      </header>
     </div>
   );
 }
