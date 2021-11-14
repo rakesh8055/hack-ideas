0cbfb6ae5d1d145168e60a2848751a8f5a4f2206
Rakesh K R <rakeshkumar8055@gmail.com>
2021-11-14 14:59:47 +0530

    Updated Homepage
    Added background image on the Home page and style corrections for responsive design

--- src/components/modules/home/Home.jsx
+++ src/components/modules/home/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import HomePageImg from '../../../images/home-page-img.jpeg';
+import './Home.styles.scss';
 
 const Home = () => (
-    <div className="container" id="home-section">
-        <div className='app-title'>
-            <h2 data-testid='header'>Hack Ideas</h2>
+    <div className="container home-section" id="home-section">
+        <div className='d-flex flex-column flex-sm-row justify-content-center align-items-center'>
+            <div className='app-title d-none d-sm-none d-md-block'>
+                <h2 data-testid='header'>Hack Ideas</h2>
+            </div>
+            <div className='app-image'>
+                <img src={HomePageImg} alt='HomePage'/>
+            </div>
         </div>
     </div>
 )
