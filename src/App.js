71f6fba62c4e661f2ffb0a34718c97d97112c799
Rakesh K R <rakeshkumar8055@gmail.com>
2021-11-14 15:33:14 +0530

    Updated Events Dashboard and used global variables for colors in the app
    Updated Events dashboard, style changes, updated colors used in the app to global variables.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.scss';
 import Navbar from './components/modules/navbar/Navbar';
 import Home from './components/modules/home/Home';
+import Eventsdashboard from './components/modules/events-dashboard/Eventsdashboard';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Home/>
+      <Eventsdashboard/>
     </div>
   );
 }
--- src/components/modules/events-dashboard/Eventsdashboard.jsx
+++ src/components/modules/events-dashboard/Eventsdashboard.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import './Eventsdashboard.styles.scss';
 
 const Eventsdashboard = () => {
-
+    return(
+    <div className='container events-dashboard'>
+        <div className='d-flex justify-content-center p-3'>
+            <h4 data-testid='events-title'>Events/Challenges</h4>
+        </div>
+    </div>)
 }
 export default Eventsdashboard;
