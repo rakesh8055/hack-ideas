e332ee173d89a987ae2b32620ac8b1a7fcbd60ba
Rakesh K R <rakeshkumar8055@gmail.com>
2021-11-14 16:59:05 +0530

    More test cases for the Card component

--- src/components/common/card/Card.test.js
+++ src/components/common/card/Card.test.js
@@ -6,13 +6,31 @@ import Card from './Card';
 
 const props = {
     title: 'title1',
-    text: 'text'
+    description: 'text',
+    tags: ['tag1'],
+    noOfVotes: 3
 }
 
-test('verify card title, text', () => {
-    const { getByTestId } = render(<Card props={props}/>)
+test('verify card title is displayed', () => {
+    const { getByTestId } = render(<Card {...props}/>);
     const title = getByTestId('card-title');
-    const text = getByTestId('card-text');
     expect(title.textContent).toBe(props.title);
+});
+
+test('verify card text is displayed', () => {
+    const { getByTestId } = render(<Card {...props}/>);
+    const text = getByTestId('card-text');
     expect(text.textContent).toBe(props.text);
+});
+
+test('verify the tags are displayed', () => {
+    const { getByTestId } = render(<Card {...props}/>);
+    const tags = getByTestId('card-tags');
+    expect(tags.textContent).toBe(props.tags[0]);
+});
+
+test('verify the votes are displayed', () => {
+    const { getByTestId } = render(<Card {...props}/>);
+    const votes = getByTestId('card-votes');
+    expect(votes.textContent).toBe(props.noOfVotes);
 });
